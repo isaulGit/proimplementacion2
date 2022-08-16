df5a62e826c29b9a85bedd0707e1bdc1fd673a39
ProBook <isaul.webgmail.com>
2022-08-15 23:12:14 -0500

    algun otro

--- app/src/main/java/com/example/proimplementacion07bfirebase/MainActivity.kt
+++ app/src/main/java/com/example/proimplementacion07bfirebase/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    //aqui
+                    //aqui fgdfg
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
